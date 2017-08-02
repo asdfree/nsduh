ce1b5ab3baa9c0bb3b13b201598e0e32cbb38a0e
Anthony Damico <ajdamico@gmail.com>
2017-08-02 12:12:13 -0400

    addhealth draft

--- tests/test.R
+++ tests/test.R
@@ -9,8 +9,8 @@ nsduh_cat <-
 		your_email = my_email_address , 
 		your_password = my_password )
 
-# 2015 only
-nsduh_cat <- subset( nsduh_cat , year == 2015 )
+# 2014 only
+nsduh_cat <- subset( nsduh_cat , temporalCoverage == 2014 )
 # download the microdata to your local computer
 stopifnot( nrow( nsduh_cat ) > 0 )
 
@@ -18,48 +18,66 @@ stopifnot( nrow( nsduh_cat ) > 0 )
 
 library(survey)
 
-nsduh_df <- readRDS( file.path( getwd() , "2015 main.rds" ) )
+nsduh_df <- 
+	readRDS( file.path( getwd() , "2014 main.rds" ) )
 
 nsduh_design <- 
 	svydesign( 
-		~ psu , 
-		strata = ~ stratum , 
+		id = ~ verep , 
+		strata = ~ vestr , 
 		data = nsduh_df , 
-		weights = ~ weight , 
+		weights = ~ analwt_c , 
 		nest = TRUE 
 	)
 nsduh_design <- 
 	update( 
 		nsduh_design , 
-		q2 = q2 ,
-		never_rarely_wore_bike_helmet = as.numeric( qn8 == 1 ) ,
-		ever_smoked_marijuana = as.numeric( qn47 == 1 ) ,
-		ever_tried_to_quit_cigarettes = as.numeric( q36 > 2 ) ,
-		smoked_cigarettes_past_year = as.numeric( q36 > 1 )
+		
+		health = 
+			factor( 
+				health , 
+				levels = 1:5 , 
+				labels = c( "excellent" , "very good" , "good" ,
+					"fair" , "poor" )
+			) ,
+			
+		age_tried_first_cigarette = ifelse( cigtry > 99 , NA , cigtry ) ,
+		
+		age_tried_cocaine = ifelse( cocage > 99 , NA , cocage ) ,
+
+		ever_used_marijuana = as.numeric( mjever == 1 ) ,
+		
+		county_type =
+			factor(
+				coutyp2 ,
+				levels = 1:3 ,
+				labels = c( "large metro" , "small metro" , "nonmetro" )
+			)
+			
 	)
 sum( weights( nsduh_design , "sampling" ) != 0 )
 
-svyby( ~ one , ~ ever_smoked_marijuana , nsduh_design , unwtd.count )
+svyby( ~ one , ~ county_type , nsduh_design , unwtd.count )
 svytotal( ~ one , nsduh_design )
 
-svyby( ~ one , ~ ever_smoked_marijuana , nsduh_design , svytotal )
-svymean( ~ bmipct , nsduh_design , na.rm = TRUE )
+svyby( ~ one , ~ county_type , nsduh_design , svytotal )
+svymean( ~ age_tried_first_cigarette , nsduh_design , na.rm = TRUE )
 
-svyby( ~ bmipct , ~ ever_smoked_marijuana , nsduh_design , svymean , na.rm = TRUE )
-svymean( ~ q2 , nsduh_design , na.rm = TRUE )
+svyby( ~ age_tried_first_cigarette , ~ county_type , nsduh_design , svymean , na.rm = TRUE )
+svymean( ~ health , nsduh_design , na.rm = TRUE )
 
-svyby( ~ q2 , ~ ever_smoked_marijuana , nsduh_design , svymean , na.rm = TRUE )
-svytotal( ~ bmipct , nsduh_design , na.rm = TRUE )
+svyby( ~ health , ~ county_type , nsduh_design , svymean , na.rm = TRUE )
+svytotal( ~ age_tried_first_cigarette , nsduh_design , na.rm = TRUE )
 
-svyby( ~ bmipct , ~ ever_smoked_marijuana , nsduh_design , svytotal , na.rm = TRUE )
-svytotal( ~ q2 , nsduh_design , na.rm = TRUE )
+svyby( ~ age_tried_first_cigarette , ~ county_type , nsduh_design , svytotal , na.rm = TRUE )
+svytotal( ~ health , nsduh_design , na.rm = TRUE )
 
-svyby( ~ q2 , ~ ever_smoked_marijuana , nsduh_design , svytotal , na.rm = TRUE )
-svyquantile( ~ bmipct , nsduh_design , 0.5 , na.rm = TRUE )
+svyby( ~ health , ~ county_type , nsduh_design , svytotal , na.rm = TRUE )
+svyquantile( ~ age_tried_first_cigarette , nsduh_design , 0.5 , na.rm = TRUE )
 
 svyby( 
-	~ bmipct , 
-	~ ever_smoked_marijuana , 
+	~ age_tried_first_cigarette , 
+	~ county_type , 
 	nsduh_design , 
 	svyquantile , 
 	0.5 ,
@@ -68,14 +86,14 @@ svyby(
 	na.rm = TRUE
 )
 svyratio( 
-	numerator = ~ ever_tried_to_quit_cigarettes , 
-	denominator = ~ smoked_cigarettes_past_year , 
+	numerator = ~ age_tried_first_cigarette , 
+	denominator = ~ age_tried_cocaine , 
 	nsduh_design ,
 	na.rm = TRUE
 )
-sub_nsduh_design <- subset( nsduh_design , qn41 == 1 )
-svymean( ~ bmipct , sub_nsduh_design , na.rm = TRUE )
-this_result <- svymean( ~ bmipct , nsduh_design , na.rm = TRUE )
+sub_nsduh_design <- subset( nsduh_design , preg == 1 )
+svymean( ~ age_tried_first_cigarette , sub_nsduh_design , na.rm = TRUE )
+this_result <- svymean( ~ age_tried_first_cigarette , nsduh_design , na.rm = TRUE )
 
 coef( this_result )
 SE( this_result )
@@ -84,8 +102,8 @@ cv( this_result )
 
 grouped_result <-
 	svyby( 
-		~ bmipct , 
-		~ ever_smoked_marijuana , 
+		~ age_tried_first_cigarette , 
+		~ county_type , 
 		nsduh_design , 
 		svymean ,
 		na.rm = TRUE 
@@ -96,22 +114,22 @@ SE( grouped_result )
 confint( grouped_result )
 cv( grouped_result )
 degf( nsduh_design )
-svyvar( ~ bmipct , nsduh_design , na.rm = TRUE )
+svyvar( ~ age_tried_first_cigarette , nsduh_design , na.rm = TRUE )
 # SRS without replacement
-svymean( ~ bmipct , nsduh_design , na.rm = TRUE , deff = TRUE )
+svymean( ~ age_tried_first_cigarette , nsduh_design , na.rm = TRUE , deff = TRUE )
 
 # SRS with replacement
-svymean( ~ bmipct , nsduh_design , na.rm = TRUE , deff = "replace" )
-svyciprop( ~ never_rarely_wore_bike_helmet , nsduh_design ,
+svymean( ~ age_tried_first_cigarette , nsduh_design , na.rm = TRUE , deff = "replace" )
+svyciprop( ~ ever_used_marijuana , nsduh_design ,
 	method = "likelihood" , na.rm = TRUE )
-svyttest( bmipct ~ never_rarely_wore_bike_helmet , nsduh_design )
+svyttest( age_tried_first_cigarette ~ ever_used_marijuana , nsduh_design )
 svychisq( 
-	~ never_rarely_wore_bike_helmet + q2 , 
+	~ ever_used_marijuana + health , 
 	nsduh_design 
 )
 glm_result <- 
 	svyglm( 
-		bmipct ~ never_rarely_wore_bike_helmet + q2 , 
+		age_tried_first_cigarette ~ ever_used_marijuana + health , 
 		nsduh_design 
 	)
 
@@ -119,17 +137,9 @@ summary( glm_result )
 library(srvyr)
 nsduh_srvyr_design <- as_survey( nsduh_design )
 nsduh_srvyr_design %>%
-	summarize( mean = survey_mean( bmipct , na.rm = TRUE ) )
+	summarize( mean = survey_mean( age_tried_first_cigarette , na.rm = TRUE ) )
 
 nsduh_srvyr_design %>%
-	group_by( ever_smoked_marijuana ) %>%
-	summarize( mean = survey_mean( bmipct , na.rm = TRUE ) )
-
-unwtd.count( ~ never_rarely_wore_bike_helmet , yrbss_design )
-
-svytotal( ~ one , subset( yrbss_design , !is.na( never_rarely_wore_bike_helmet ) ) )
- 
-svymean( ~ never_rarely_wore_bike_helmet , yrbss_design , na.rm = TRUE )
-
-svyciprop( ~ never_rarely_wore_bike_helmet , yrbss_design , na.rm = TRUE , method = "beta" )
+	group_by( county_type ) %>%
+	summarize( mean = survey_mean( age_tried_first_cigarette , na.rm = TRUE ) )
 
