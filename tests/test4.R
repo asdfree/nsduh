ba114c7ab6afdab1588fb1cbe3c7f053cc331862
Anthony Damico <ajdamico@gmail.com>
2018-01-30 10:17:31 -0500

    appveyor timeout tests

--- tests/test4.R
+++ tests/test4.R
@@ -39,6 +39,47 @@ nsduh_design <-
 		nest = TRUE 
 	)
 print('svydesign finished')
+if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
+
+options("lodown.cachaca.savecache"=FALSE)
+
+this_sample_break <- Sys.getenv( "this_sample_break" )
+
+library(lodown)
+
+nsduh_cat <-
+	get_catalog( "nsduh" ,
+		output_dir = file.path( getwd() ) )
+
+nsduh_cat <- nsduh_cat[ split( seq( nrow( nsduh_cat ) ) , 1 + sort( seq( nrow( nsduh_cat ) ) %% 5 ) )[[ this_sample_break ]] , ]
+
+if( any( nsduh_cat$year == 2016 ) ){
+library(lodown)
+# examine all available NSDUH microdata files
+nsduh_cat <-
+	get_catalog( "nsduh" ,
+		output_dir = file.path( getwd() ) )
+print('get_catalog two finished')
+
+# 2016 only
+nsduh_cat <- subset( nsduh_cat , year == 2016 )
+# download the microdata to your local computer
+
+
+library(survey)
+
+nsduh_df <- 
+	readRDS( file.path( getwd() , "2016 main.rds" ) )
+print('readRDS finished')
+nsduh_design <- 
+	svydesign( 
+		id = ~ verep , 
+		strata = ~ vestr , 
+		data = nsduh_df , 
+		weights = ~ analwt_c , 
+		nest = TRUE 
+	)
+print('svydesign finished')
 nsduh_design <- 
 	update( 
 		nsduh_design , 
@@ -106,6 +147,7 @@ svyratio(
 sub_nsduh_design <- subset( nsduh_design , preg == 1 )
 
 print('subset finished')
+
 svymean( ~ age_tried_first_cigarette , sub_nsduh_design , na.rm = TRUE )
 this_result <- svymean( ~ age_tried_first_cigarette , nsduh_design , na.rm = TRUE )
 
@@ -139,6 +181,7 @@ svyciprop( ~ ever_used_marijuana , nsduh_design ,
 svyttest( age_tried_first_cigarette ~ ever_used_marijuana , nsduh_design )
 
 print('svyttest finished')
+
 svychisq( 
 	~ ever_used_marijuana + health , 
 	nsduh_design 
@@ -152,13 +195,5 @@ glm_result <-
 
 summary( glm_result )
 print('glm_result finished')
-library(srvyr)
-nsduh_srvyr_design <- as_survey( nsduh_design )
-nsduh_srvyr_design %>%
-	summarize( mean = survey_mean( age_tried_first_cigarette , na.rm = TRUE ) )
-
-nsduh_srvyr_design %>%
-	group_by( county_type ) %>%
-	summarize( mean = survey_mean( age_tried_first_cigarette , na.rm = TRUE ) )
 
 }
